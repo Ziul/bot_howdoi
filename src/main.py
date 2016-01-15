a5a11007f6566e1a1fa8392adc0e67ff1d4a4c5a
Luiz Oliveira <ziuloliveira@gmail.com>
2016-01-15 02:01:10 -0200

    How do i base inline
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# from telegram import Updater, Update,
-# InlineQueryResultPhoto, ParseMode, InlineQueryResultArticle
-from telegram import Updater, InlineQueryResultPhoto, ChatAction
-from globals import *
+# from telegram import ParseMode
+from telegram import Updater, ChatAction, InlineQueryResultArticle
+from globals import token, logger
+import re
 from howdoi.howdoi import howdoi as howdoi_call
+from random import getrandbits
 
 
 def main():
@@ -43,40 +44,32 @@ def error(bot, update, error):
         update.first_name, error))
 
 
+def escape_markdown(text):
+    """Helper function to escape telegram markup symbols"""
+    escape_chars = '\*_`\['
+    return re.sub(r'([%s])' % escape_chars, r'\\\1', text)
+
+
 def inlinequery(bot, update):
-    logger.info('Inline query')
+    result = ''
+    _hex = hex(getrandbits(64))[2:]
+    results = list()
+    logger.info('Inline: ' + update.inline_query.query)
     if update.inline_query is not None and update.inline_query.query:
-        api = "https://api.deckbrew.com/mtg/cards/typeahead?q=<NAME>"
-        query = update.inline_query.query.split("//")[0].strip()
-        logger.debug("Query is :{0}".format(query))
-        cards = requests.get(api.replace(
-            "<NAME>", query.lower().replace(",", "")).replace("'", "")).json()
-        images = list()
-        i = 0
-        if(cards == []):
-            return
-        if (len(cards) == 1):
-            images.append(InlineQueryResultPhoto(
-                id=hex(getrandbits(64))[2:], photo_url=cards[
-                    0]['editions'][0]['image_url'],
-                thumb_url=cards[0]['editions'][0]['image_url']))
-            logger.debug("Card {0} : {1}".format(
-                cards[0]['name'], cards[0]['editions'][0]['image_url']))
-            bot.answerInlineQuery(update.inline_query.id, results=images)
-            return
-        for card in cards:
-            img = ''
-            if(card['editions'][0]['multiverse_id'] == 0):
-                img = card['editions'][1]['image_url']
-            else:
-                img = card['editions'][0]['image_url']
-            images.append(InlineQueryResultPhoto(id=i, caption=card['name'],
-                                                 title=card['name'],
-                                                 photo_url=img,
-                                                 thumb_url=img))
-            logger.debug("Card {0} {1} : {2}".format(i, card['name'], img))
-            i = i + 1
-        bot.answerInlineQuery(update.inline_query.id, results=images)
+        query = update.inline_query.query.split(' ')
+        result = howdoi(bot, update, query)
+        results.append(
+            InlineQueryResultArticle(
+                id=_hex,
+                title="StackOverflow",
+                message_text="*%s*" % query,
+                # parse_mode=ParseMode.MARKDOWN,
+            )
+        )
+
+        bot.answerInlineQuery(update.inline_query.id, results=results)
+    logger.debug('result=' + result)
+    return results
 
 
 def howdoi(bot, update, args):
@@ -90,19 +83,22 @@ def howdoi(bot, update, args):
             'query': args}
     result = howdoi_call(args).encode('utf-8', 'ignore')
     logger.debug(result)
-    bot.sendMessage(chat_id, result)
+    if 'inline_query' not in update.to_dict().keys():
+        bot.sendMessage(chat_id, result)
+    return result
 
 if __name__ == '__main__':
     main()
 
 
 def test():
+    logger.info('Testing mode')
     from sys import argv
+    from mock import Mock
     argv = argv[1:]
-    args = {'all': False,
-            'color': False,
-            'num_answers': 1,
-            'pos': 1,
-            'query': argv}
-    result = howdoi_call(args).encode('utf-8', 'ignore')
-    print(result)
+    bot = Mock()
+    update = Mock()
+    update.inline_query.query = argv
+    update.to_dict().keys = Mock(return_value={'message': argv})
+    result = howdoi(bot, update, argv)
+    print('\n' + result)
